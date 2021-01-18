050f6e82bead1945621aa8c65b0da0f2d0ac3c44
el fatmi mohamed <e.mohamed@globaletik.com>
2021-01-18 20:20:52 +0100

    feat/admin

--- resources/views/admin/layouts/admin_layout.blade.php
+++ resources/views/admin/layouts/admin_layout.blade.php
@@ -19,23 +19,6 @@
     <link type="text/css" href="{{ asset('admin') }}/css/argon.css?v=1.0.0" rel="stylesheet">
     <!-- Extra details for Live View on GitHub Pages -->
     @stack('css')
-
-    <script>
-        (function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
-    </script>
     <!-- End Google Tag Manager -->
 </head>
 <body class="">
